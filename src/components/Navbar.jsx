066c3d51736e2fea033f307ddf00969212b75f49
Dan <daniel.speranskiy@live.com>
2024-03-29 11:18:00 +0400

    still fixing the text size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -84,13 +84,15 @@ const Navbar = () => {
                     active !== Link.title &&
                     "text-white/60 hover:text-white transition-all duration-300 ease-in-out"
                   }
-                  font-poppins font-bold text-5xl tracking-wide cursor-pointer text-[16px] hover:underline`}
+                  font-poppins font-bold  tracking-wide cursor-pointer text-[16px] hover:underline`}
                   onClick={() => {
                     setActive(Link.title);
                     setToggle(!toggle);
                   }}
                 >
-                  <a href={`#${Link.id}`}>{Link.title}</a>
+                  <a href={`#${Link.id}`} className="text-4xl">
+                    {Link.title}
+                  </a>
                 </li>
               ))}
             </ul>
