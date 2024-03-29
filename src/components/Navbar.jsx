5f81a0ca41f3a94cb85bb791b112cea23f8ffa52
Dan <daniel.speranskiy@live.com>
2024-03-29 19:42:56 +0400

    added animation to the mobile menu and hamburger thingy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import FancyText from "@carefully-coded/react-text-gradient";
 import DarkModeToggle from "./DarkModeToggle";
-
+import { Sling as Hamburger } from "hamburger-react";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { menu, close } from "../assets";
-
+import { motion, AnimatePresence } from "framer-motion";
 const Navbar = () => {
-  const [active, setActive] = useState('');
+  const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
@@ -18,6 +17,11 @@ const Navbar = () => {
       document.body.classList.remove("overflow-hidden");
     }
   }, [toggle]);
+
+  const variants = {
+    hidden: { x: "100vh", opacity: 0 },
+    visible: { x: 0, opacity: 1 },
+  };
 
   return (
     <nav
@@ -64,39 +68,47 @@ const Navbar = () => {
           ))}
         </ul>
         <div className="lg:hidden flex flex-1 justify-end items-center">
-          <img
-            src={toggle ? close : menu}
-            alt="menu"
-            className="w-[28px] h-[28px] object-contain cursor-pointer "
-            onClick={() => setToggle(!toggle)}
+          <Hamburger
+            toggled={toggle}
+            toggle={() => setToggle(!toggle)}
+            color="#C99EC0"
           />
-          <div
-            className={`${
-              !toggle ? "hidden fade-in-out" : " fade-in-out"
-            } px-12 py-5 bg-gradient-to-b bg-black/70 from-black flex to-transparent absolute w-full items-center h-screen justify-center top-16 right-0 my-0 `}
-          >
-            <ul className=" list-none text-white mb-80 flex justify-center text-center items-center flex-col gap-16 ">
-              {navLinks.map((Link) => (
-                <li
-                  key={Link.id}
-                  className={`${active === Link.title ? "text-white" : ""} 
+          <AnimatePresence>
+            {toggle && (
+              <motion.div
+                variants={variants} // Pass the variants object
+                initial="hidden" // Set the initial state to the 'hidden' variant
+                animate="visible" // Set the animate state to the 'visible' variant
+                exit="hidden" // Set the exit state to the 'hidden' variant
+                transition={{ type: "", stiffness: 100 }}
+                className={`${
+                  !toggle ? "hidden" : ""
+                } px-12 py-5 bg-gradient-to-b bg-black/70 from-black flex to-transparent absolute w-full items-center h-screen justify-center top-20 right-0 my-0`}
+              >
+                <ul className=" list-none text-white mb-80 flex justify-center text-center items-center flex-col gap-16 ">
+                  {navLinks.map((Link) => (
+                    <li
+                      key={Link.id}
+                      className={`${active === Link.title ? "text-white" : ""} 
                   ${
                     active !== Link.title &&
                     "text-white/60 hover:text-white transition-all duration-300 ease-in-out"
                   }
-                  font-poppins font-bold  tracking-wide cursor-pointer text-[16px] hover:underline`}
-                  onClick={() => {
-                    setActive(Link.title);
-                    setToggle(!toggle);
-                  }}
-                >
-                  <a href={`#${Link.id}`} className="text-4xl">
-                    {Link.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
+                  font-poppins font-bold  tracking-wide cursor-pointer text-[16px]`}
+                      onClick={() => {
+                        setActive(Link.title);
+                        setToggle(!toggle);
+                      }}
+                    >
+                      <a href={`#${Link.id}`} className="text-4xl">
+                        {Link.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
     </nav>
